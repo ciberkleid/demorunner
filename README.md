ac9465461a4d1317f4cc0b0feea41982aa70bf0a
Cora Iberkleid <ciberkleid@gmail.com>
2020-06-24 13:36:02 -0400

    Use # to skip lines. Use @_ECHO_# to echo as comments

--- README.md
+++ README.md
@@ -1,15 +1,71 @@
-# demorunner
-sh script to automate shell controlled demos.
+# demorunner and demos scripts
 
-This script executes a sequence of shell commands saved in a separate file 
-(this file name is provided as input paramater for the script).  
+## Setup
 
-The goal of the script if to offer a pre-packaged demo experience controlled by the the operartor with 
-control over speed and text colors for a better clarity for the final user.
+Create your demo scripts and any supporting files.
+Place them in the corresponding subdirectories, as show in the example below:
 
-The inital version allow for execution of extra commands during the demo and the possibility to start 
-the demo form a specific line number (where feasible).
+_demoscripts/demos/**mydemo**.txt_
 
-Run the script without arguments to show the help.
+_demoscripts/files/**mydemo**_
 
- 
+## Execution
+
+To run a demo script, execute the following command:
+
+   ```
+   source run.sh demos/mydemo.txt [files/mydemo] [-f] [-a]
+   ```
+   
+   The run.sh script will create a temp directory to execute the demo script:
+   
+   _demoscripts/temp/**mydemo**_
+   
+   `run.sh` will create some env vars and aliases that might be handy in your demo scripts. For example:
+   
+   ```
+   DEMO_HOME=<directory from which you are executing run.sh>
+   DEMO_TEMP=<temp directory for execution of demo script>
+   DEMO_FILES=<files directory you provided to run script>
+   ```
+   
+   Look through `run.sh` for more.
+   
+   The optional `-f` flag instructs `run.sh` to delete and recreate the temp directory.
+   
+   The optional `-m` flag instructs `run.sh` not to run demorunner automatically after setting up the env. You can run demorunner manually using:
+   
+   `cd ${DEMO_TEMP}; source demorunner.sh ${DEMO_SCRIPT} 1; cd ${DEMO_HOME}`
+   
+   As a convenience, `run.sh` will also place the demorunner command in in your clipboard, so you can just paste it into the terminal window.
+   
+   To start your demo at an arbitrary line in your script, change the 1 to the appropriate line number.
+      
+
+## Dependencies
+
+The `run.sh` assumes you have the following utilities installed:
+
+Required:
+- coreutils (for greadlink)
+
+Optional*:
+- [bat](https://github.com/sharkdp/bat)
+- [colordiff](https://www.colordiff.org)
+
+ .* Some of the aliases created in `run.sh` use `bat` and `colordiff`. The utilities are only required if you plan to use these utilities of the aliases in your scripts. If you are going to use the demo scripts included in this repo, then you will need these utilities.
+
+On Mac OS, you can install the above utilities using:
+```
+brew install coreutils
+brew install bat
+brew install colordiff
+```
+
+
+   
+-----
+
+For more information on using demorunner.sh, type:
+
+`demorunner.sh -h`
---
+++ run.sh
@@ -0,0 +1,202 @@
+# Usage example:
+# source run.sh demos/dockerfile.txt files/dockerfile -f
+# OR
+# source run.sh demos/dockerfile.txt files/dockerfile -f -m
+# source demorunner.sh demos/dockerfile.txt 1
+
+# Default values of arguments
+demo_script=""
+demo_files=""
+force_cleanup_enabled=0
+run_demorunner_enabled=1
+
+# Check number of arguments
+if [ "$#" -gt 4 ]; then
+    echo "Illegal number of arguments"
+    echo "Usage:"
+    echo "source run.sh <script_file> <files_dir> [-f] [-m]"
+    echo "Notes: -f forces deletion and recreation of demo temp directory"
+    echo "       -m disables calling demorunner automatically"
+    kill -INT $$
+fi
+
+# Process arguments
+# Check for a "-f" boolean flag to enable forced cleanup and avoid being prompted
+# Inspired by: https://pretzelhands.com/posts/command-line-flags
+for arg in "$@"
+do
+    case $arg in
+        -f|--force)
+        force_cleanup_enabled=1
+        shift # Remove from processing
+        ;;
+        -m|--manual)
+        run_demorunner_enabled=0
+        shift
+        ;;
+        *)
+        if [[ "${demo_script}" == "" ]]; then
+          demo_script="${1}"
+          if [ ! -f "${demo_script}" ]; then
+            echo "File does not exist: [${demo_script}]"
+            kill -INT $$
+          fi
+        elif [[ "${demo_files}" == "" ]]; then
+          demo_files="${1}"
+          if [ ! -d "${demo_files}" ]; then
+            echo "Directory does not exist: [${demo_files}]"
+            kill -INT $$
+          fi
+        fi
+        shift
+        ;;
+    esac
+done
+
+echo -e "\n\n##### SETTING UP DEMO [${demo_script}] [${demo_files}] [$(date)] #####"
+
+# brew install coreutils (for greadlink)
+demo_script_absolute_path=$(greadlink -f "${demo_script}")
+demo_script_handle=$(echo $(basename "${demo_script}") | cut -d. -f1)
+if [[ "${demo_files}" != "" ]]; then
+  demo_files_absolute_path=$(greadlink -f "${demo_files}")
+else
+  demo_files_absolute_path=""
+fi
+
+##### DEMO ENV VARS
+export DEMO_HOME=`pwd`
+export DEMO_SCRIPT="${demo_script_absolute_path}"
+export DEMO_FILES="${demo_files_absolute_path}"
+export DEMO_TEMP="${DEMO_HOME}/temp/${demo_script_handle}"
+# Default delay is 10. To make it faster, increase the number
+export DEMO_DELAY=${DEMO_DELAY:-15}
+export SAVED_DEMO_DELAY=${DEMO_DELAY}
+
+##### APPEARANCE SETTINGS
+# https://github.com/sharkdp/bat
+#brew install bat
+
+if [[ $(which bat) != "" ]]; then
+  echo -e "\nSetting up bat utility"
+  mkdir -p "$(bat --config-dir)/themes"
+  cp config/bat/themes/*.tmTheme "$(bat --config-dir)/themes"
+  bat cache --build
+  export BAT_STYLE=grid
+  #export BAT_STYLE=plain
+  #export BAT_STYLE=numbers
+  export BAT_PAGER=""
+  #export BAT_PAGER="never"
+fi
+
+if [ -z ${COLORFGBG} ]; then
+  # Background is white
+  export BAT_THEME=ansi-light-MODIFIED
+  #export BAT_THEME=GitHub
+  export DEMO_COLOR=blue
+else
+  # Background is black
+  if [[ ${DEMO_COLOR} != white ]]; then
+    export DEMO_COLOR=yellow
+  fi
+fi
+
+##### TEMP DIR
+
+echo -e "\nSetting up temp dir [${DEMO_TEMP}]"
+if [ "$(ls -A ${DEMO_TEMP})" ]; then
+  echo "Temp dir contents"
+  tree "${DEMO_TEMP}"
+fi
+if [ ${force_cleanup_enabled} -eq 1 ]; then
+  echo "Forced deletion is enabled. Recreating temp directory."
+  rm -rf "${DEMO_TEMP}"
+  echo "Temp dir conents"
+  tree "${DEMO_TEMP}"
+else
+  echo "Forced deletion is not enabled. Using existing temp directory."
+fi
+mkdir -p "${DEMO_TEMP}"
+
+##### ALIASES
+
+# https://github.com/dandavison/delta
+#brew install git-delta
+
+# Stop running containers & prune images, containers, volumes, and networks (stopped, unused, and dangling)
+alias dclean="docker ps -a -q | xargs -n1 docker stop; docker system prune -af"
+# Remove all containers; prune dangling images; prune images, containers, volumes, and networks with specified label
+alias dclean2="docker ps -a -q | xargs -n1 docker rm -f; docker image prune -f; docker system prune -af --filter label=maintainer=me@example.com"
+
+# Change Terminal prompt to show only a $
+export PS1="\[\033[0m\]\$ "
+
+# BEGIN SECTION: Fancy cat and diff aliases
+#brew install colordiff
+
+# catt - like cat, but skip commented lines and empty lines
+cattf() { grep -v -A1 '^[[:blank:]]*$' "${@}" | grep -v '^--$' | grep -vE '^\s*#'; }
+alias catt=cattf
+
+# cattd - like dif, but side-by-side and colored, and skip commented lines and empty lines
+cattdf() { cattf ${1} > .___cattdf_temp_file_1; cattf ${2} > .___cattdf_temp_file_2; colordiff -yW"`tput cols`" .___cattdf_temp_file_1 .___cattdf_temp_file_2; rm .___cattdf_temp_file_1; rm .___cattdf_temp_file_2; }
+alias cattd=cattdf
+
+# catd - like dif, but side-by-side and colored
+catdf() { colordiff -yW"`tput cols`" ${1} ${2}; }
+alias catd=catdf
+# END SECTION: Fancy cat and diff aliases
+
+# Generate args to highlight changed lines for bat
+BAT_LANG=""
+batdf() { hArgs=$(diff --unchanged-line-format="" --old-line-format="" --new-line-format="%dn " ${1} ${2} | xargs -n1 -I {} printf -- '-H %s:%s ' {} {}); bat ${BAT_LANG} ${2} $hArgs; }
+alias batd=batdf
+setBatLangf() { if [[ "${1}" == "" ]]; then export BAT_LANG=""; else export BAT_LANG="-l ${1}"; fi; alias bat="bat ${BAT_LANG}"; }
+alias setBatLang=setBatLangf
+setBatLang ""
+# Usage example:
+# setBatLang Dockerfile
+# bat Dockerfile
+# batd Dockerfile Dockerfile2
+# setBatLang exclude
+# bat .dockerignore
+# batd .dockerignore .dockerignore2
+# To use default language detection, set to empty string:
+# setBatLang
+
+#####  PRINT ENV VARS
+
+echo -e "\nDemo config:"
+echo "DEMO_HOME=${DEMO_HOME}"
+echo "DEMO_TEMP=${DEMO_TEMP}"
+echo "DEMO_SCRIPT=${DEMO_SCRIPT}"
+echo "DEMO_FILES=${DEMO_FILES}"
+echo "DEMO_DELAY=${DEMO_DELAY}"
+echo "SAVED_DEMO_DELAY=${SAVED_DEMO_DELAY}"
+echo "DEMO_COLOR=${DEMO_COLOR}"
+if [[ $(which bat) != "" ]]; then
+  echo "BAT_STYLE=${BAT_STYLE}"
+  echo "BAT_PAGER=${BAT_PAGER}"
+  echo "BAT_THEME=${BAT_THEME}"
+  echo "BAT_LANG=${BAT_LANG}   # to change, use: setBatLang <language>"
+fi
+echo -e "\nFinished setting up environment"
+
+#####  PROVIDE COMMAND FOR STARTING DEMO SCRIPT
+command="cd \${DEMO_TEMP}; source \${DEMO_HOME}/demorunner.sh \${DEMO_SCRIPT} 1; cd \${DEMO_HOME}"
+printf "${command}" | pbcopy
+echo
+echo "To start the demo, execute the following command (it's in your clipboard!):"
+echo "cd \${DEMO_TEMP}; source \${DEMO_HOME}/demorunner.sh \${DEMO_SCRIPT} 1; cd \${DEMO_HOME}"
+echo
+echo "Expanded form:"
+echo "cd ${DEMO_TEMP}; source ${DEMO_HOME}/demorunner.sh ${DEMO_SCRIPT} 1; cd ${DEMO_HOME}"
+echo
+
+##### RUN AUTOMATICALLY
+if [ ${run_demorunner_enabled} -eq 1 ]; then
+  echo -e "\nExecuting ${DEMO_HOME}/demorunner.sh...\n"
+  cd "${DEMO_TEMP}"
+  source ${DEMO_HOME}/demorunner.sh "${DEMO_SCRIPT}" 1
+  cd "${DEMO_HOME}"
+fi
